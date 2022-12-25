a8119258616746e812284eca105ff03661ffc3bd
DragunWF <plarisanmarc@gmail.com>
2022-12-25 21:27:16 +0800

    Updated DebugTool.java - Implemented view methods

--- src/main/java/dragunwf/quickmath/DebugTool.java
+++ src/main/java/dragunwf/quickmath/DebugTool.java
@@ -1,5 +1,19 @@
 package dragunwf.quickmath;
 
+import java.util.ArrayList;
+
 public class DebugTool {
+    public static void displayArrayList(ArrayList<String> arr) {
+        for (int i = 0, n = arr.size(); i < n; i++) {
+            System.out.printf("%s: %s ", i, arr.get(i));
+        }
+        System.out.println();
+    }
     
+    public static void displayCharArray(char[] arr) {
+        for (char chr : arr) {
+            System.out.printf("%s ", chr);
+        }
+        System.out.println();
+    }
 }
