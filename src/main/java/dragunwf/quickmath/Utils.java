88399d6fa1db59c6d01f4f692854c595beee44ae
DragunWF <plarisanmarc@gmail.com>
2022-12-25 21:27:01 +0800

    Updated Utils.java - Implemented formatNumber() method

--- src/main/java/dragunwf/quickmath/Utils.java
+++ src/main/java/dragunwf/quickmath/Utils.java
@@ -3,17 +3,31 @@ package dragunwf.quickmath;
 import java.util.*;
 
 public class Utils {
-    public static void formatNumber(Integer number) {
+    public static String formatNumber(Integer number) {
         char[] numCharArr = number.toString().toCharArray();
-        Arrays.sort(numCharArr);
-        String strNum = number.toString();
-        ArrayList<String> output = new ArrayList<String>();
-        
+        numCharArr = reverseCharArray(numCharArr);
+
+        String strNum = String.valueOf(numCharArr);
+        ArrayList<String> output = new ArrayList<>();
+
         for (int i = 0, n = strNum.length(); i < n; i++) {
             output.add(Character.toString(strNum.charAt(i)));
-            if (i + 1 % 3 == 0) {
+            if ((i + 1) % 3 == 0) {
                 output.add(",");
             }
         }
+        
+        char[] resultCharArray = String.join("", output).toCharArray();
+        resultCharArray = reverseCharArray(resultCharArray);
+
+        return String.valueOf(resultCharArray);
+    }
+
+    private static char[] reverseCharArray(char[] arr) {
+        char[] output = new char[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            output[arr.length - (i + 1)] = arr[i];
+        }
+        return output;
     }
 }
