f5816637ba8fd8879f04b7ef8c8206fab94bea7f
DragunWF <plarisanmarc@gmail.com>
2023-01-11 21:05:44 +0800

    Updated Data.java

--- src/main/java/dragunwf/quickmath/Data.java
+++ src/main/java/dragunwf/quickmath/Data.java
@@ -4,11 +4,15 @@ public class Data {
     private static int gamesPlayedInSession = 0;
     private static int highScore = 0;
     
-    public static void saveScore(int newScore) {
+    public static void saveScore(int score) {
         gamesPlayedInSession++;
-        if (newScore > highScore) {
-            highScore = newScore;
+        if (score > highScore) {
+            highScore = score;
         }
+    }
+    
+    public static int getGamesPlayed() {
+        return gamesPlayedInSession;
     }
     
     public static int getHighScore() {
