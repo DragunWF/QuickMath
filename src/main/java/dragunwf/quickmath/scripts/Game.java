23affe03dce521b6823775832bb309b2fcdcb018
Marc Plarisan <plarisanmarc@gmail.com>
2023-01-12 16:27:22 +0800

    Added Game.java and implemented base mechanics

---
+++ src/main/java/dragunwf/quickmath/scripts/Game.java
@@ -0,0 +1,33 @@
+package dragunwf.quickmath.scripts;
+
+public class Game {
+    private static String equationText = null;
+    private static int correctAnswer;
+    
+    public static void randomizeEquation() {
+        int a = getRandomNum(5, 150); 
+        int b = getRandomNum(5, 150);
+        equationText = String.format("%s + %s", a, b);
+        correctAnswer = a + b;
+    }
+    
+    public static String getEquationText() throws Exception {
+        validateEquation();
+        return equationText;
+    }
+    
+    public static int getCorrectAnswer() throws Exception {
+        validateEquation();
+        return correctAnswer;
+    }
+    
+    private static void validateEquation() throws Exception {
+        if (equationText == null) {
+            throw new Exception("Call randomizeEquation() first!");
+        }
+    }
+    
+    private static int getRandomNum(int min, int max) {
+        return (int) Math.floor(Math.random() * (max - min) + min);
+    }
+}
