1053e1a8846e63640a5e4835493c1b8b76399b68
DragunWF <plarisanmarc@gmail.com>
2023-01-12 23:35:30 +0800

    Updated WindowManager.java Implemented validation

--- src/main/java/dragunwf/quickmath/scripts/WindowManager.java
+++ src/main/java/dragunwf/quickmath/scripts/WindowManager.java
@@ -3,11 +3,20 @@ package dragunwf.quickmath.scripts;
 import dragunwf.quickmath.ui.GameUI;
 import dragunwf.quickmath.ui.MainMenuUI;
 import dragunwf.quickmath.ui.RetryMenuUI;
+import java.util.HashMap;
 
 public class WindowManager {
+    private static HashMap<String, Boolean> windowsOpened;
     private static MainMenuUI mainMenuUI;
     private static GameUI gameUI;
     private static RetryMenuUI retryMenuUI;
+    
+    public static void initialize() {
+        windowsOpened = new HashMap<>();
+        windowsOpened.put("mainMenu", false);
+        windowsOpened.put("game", false);
+        windowsOpened.put("retryMenu", false);
+    }
     
     public static void openMainMenu() throws Exception {
         WindowManager.openWindow("mainMenu");
@@ -22,6 +31,10 @@ public class WindowManager {
     }
     
     private static void openWindow(String name) throws Exception {
+        if (windowsOpened.get(name)) {
+            return; // avoids window duplication
+        }
+            
         switch (name) {
             case "mainMenu" -> {
                 mainMenuUI = new MainMenuUI();
@@ -37,6 +50,14 @@ public class WindowManager {
             }
             default -> throw new Exception("Window name passed is not recognized!");
         }
+        
+        windowsOpened.put(name, true);
+        for (String key : windowsOpened.keySet()) {
+            if (!key.equals(name)) {
+                windowsOpened.put(key, false);
+            }
+        }
+        
         System.out.printf("Opened %sUI\n", name);
     }
 }
